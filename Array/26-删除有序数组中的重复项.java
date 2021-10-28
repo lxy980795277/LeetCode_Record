06b69ae855ed7f6ce52090763834fca81198f085
lxy15238330738 <liu15238330738@163.com>
2021-10-28 22:51:15 +0800

    10-28 pc

--- Array/26-删除有序数组中的重复项.java
+++ Array/26-删除有序数组中的重复项.java
@@ -19,7 +19,28 @@ class Solution {
     }
 }
 
-/*双指针解法
+//*双指针解法
+//思路 用双指针来解决，i指针用于遍历数组，j指针用于虚构一个结果数组，始终指向有效数组末尾
+//j初始位置指向0，即原数组的开头，相当于把数组的第一个元素添加进了结果数组，
+//然后i指针向后移动，如果和j指针末尾的数不相同，就添加进来，然后j指针继续向后移动
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int len = nums.length;
+        int j = 0;       //指向有效数组的最后一位 
+        for (int i = 0; i < len; i++) {
+            if (nums[i] != nums[j]) {
+                j++;
+                nums[j] = nums[i];
+            }
+            else{
+                continue;
+            }
+        }
+        return j + 1;
+    }
+}
+
+
 
 
 
